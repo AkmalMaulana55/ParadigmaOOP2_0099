c5f8d43bd92f635816af4fd70e16db85e8b6daab
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-06-03 14:58:42 +0700

    int main enkapsulasi

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,19 @@ public:
         return saklarNo[i];
     }
 };
+
+int main() {
+    remoteLampu LampuRumah;
+
+    LampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    LampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    LampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    LampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << LampuRumah.setSaklarNo(0) << endl;
+    cout << LampuRumah.setSaklarNo(1) << endl;
+    cout << LampuRumah.setSaklarNo(2) << endl;
+    cout << LampuRumah.setSaklarNo(3) << endl;
+
+    return 0;
+}
